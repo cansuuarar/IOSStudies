aedb1cada11e9a97be3d2052fea9e742931cf08f
Cansua <cansuarar1@gmail.com>
2024-10-17 11:02:59 +0300

    alamofire request added
    alamofire request added

--- APIExample/APIExample/ViewController.swift
+++ APIExample/APIExample/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request("https://meowfacts.herokuapp.com/?count=3").response { response in
+            debugPrint(response)
+        }
     }
-
-
 }
 
